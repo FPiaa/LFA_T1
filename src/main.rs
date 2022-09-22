ff93254da5553c2f6384d23c86401a580c2cf9f5
Igor Fidler <igorgfidler@hotmail.com>
2022-09-21 22:49:07 -0300

    pronto

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
+use std::io::Read;
 use clap::Parser;
 use std::process;
 use std::{
     collections::HashSet,
     fs::File,
-    io::{self, BufReader, Read},
-    path::Path,
     str::FromStr,
 };
 
@@ -124,6 +123,19 @@ struct Labirinto<'a> {
 fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
     use Estado::*;
     use Símbolo::*;
+
+    // estados A* -> E* depois de pegar o tesouro
+    // estados B* -> F* depois de pegar o tesouro
+    // estados C* -> G* depois de pegar o tesouro
+    // estados D* -> H* depois de pegar o tesouro
+
+    //   a b c d
+    // 1
+    // 2
+    // 3
+    // 4
+    //   e f g h
+
     match (estado_atual, simbolo) {
         (A1, Cima) => Some(A2),
         (A1, Direita) => Some(B1),
@@ -137,11 +149,11 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (A2, Baixo) => Some(A1),
         (A2, Pegar) => Some(A2),
 
-        (A3, Cima) => Some(A4),
-        (A3, Direita) => Some(B3),
-        (A3, Esquerda) => Some(A3),
-        (A3, Baixo) => Some(A2),
-        (A3, Pegar) => Some(A3),
+        (A3, Cima) => None,
+        (A3, Direita) => None,
+        (A3, Esquerda) => None,
+        (A3, Baixo) => None,
+        (A3, Pegar) => None,
 
         (A4, Cima) => Some(A4),
         (A4, Direita) => Some(B4),
@@ -165,7 +177,7 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (B3, Direita) => Some(C3),
         (B3, Esquerda) => Some(A3),
         (B3, Baixo) => Some(B2),
-        (B3, Pegar) => Some(B3),
+        (B3, Pegar) => Some(F3),
 
         (B4, Cima) => Some(B4),
         (B4, Direita) => Some(C4),
@@ -173,11 +185,11 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (B4, Baixo) => Some(B3),
         (B4, Pegar) => Some(B3),
 
-        (C1, Cima) => Some(C2),
-        (C1, Direita) => Some(D1),
-        (C1, Esquerda) => Some(B1),
-        (C1, Baixo) => Some(C1),
-        (C1, Pegar) => Some(C1),
+        (C1, Cima) => None,
+        (C1, Direita) => None,
+        (C1, Esquerda) => None,
+        (C1, Baixo) => None,
+        (C1, Pegar) => None,
 
         (C2, Cima) => Some(C3),
         (C2, Direita) => Some(D2),
@@ -185,11 +197,11 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (C2, Baixo) => Some(C1),
         (C2, Pegar) => Some(C2),
 
-        (C3, Cima) => Some(C4),
-        (C3, Direita) => Some(D3),
-        (C3, Esquerda) => Some(B3),
-        (C3, Baixo) => Some(C2),
-        (C3, Pegar) => Some(C3),
+        (C3, Cima) => None,
+        (C3, Direita) => None,
+        (C3, Esquerda) => None,
+        (C3, Baixo) => None,
+        (C3, Pegar) => None,
 
         (C4, Cima) => Some(C4),
         (C4, Direita) => Some(D4),
@@ -206,7 +218,7 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (D2, Cima) => Some(D3),
         (D2, Direita) => Some(D2),
         (D2, Esquerda) => Some(C2),
-        (D2, Baixo) => Some(D2),
+        (D2, Baixo) => Some(D1),
         (D2, Pegar) => Some(D2),
 
         (D3, Cima) => Some(D4),
@@ -215,11 +227,11 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (D3, Baixo) => Some(D2),
         (D3, Pegar) => Some(D3),
 
-        (D4, Cima) => Some(D4),
-        (D4, Direita) => Some(D4),
-        (D4, Esquerda) => Some(C4),
-        (D4, Baixo) => Some(D3),
-        (D4, Pegar) => Some(D4),
+        (D4, Cima) => None,
+        (D4, Direita) => None,
+        (D4, Esquerda) => None,
+        (D4, Baixo) => None,
+        (D4, Pegar) => None,
 
         (E1, Cima) => Some(E2),
         (E1, Direita) => Some(F1),
@@ -233,11 +245,12 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (E2, Baixo) => Some(E1),
         (E2, Pegar) => Some(E2),
 
-        (E3, Cima) => Some(E4),
-        (E3, Direita) => Some(F3),
-        (E3, Esquerda) => Some(E3),
-        (E3, Baixo) => Some(E2),
-        (E3, Pegar) => Some(E3),
+        // Wumpus te pegou na volta
+        (E3, Cima) => None,
+        (E3, Direita) => None,
+        (E3, Esquerda) => None,
+        (E3, Baixo) => None,
+        (E3, Pegar) => None,
 
         (E4, Cima) => Some(E4),
         (E4, Direita) => Some(F4),
@@ -269,11 +282,11 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (F4, Baixo) => Some(F3),
         (F4, Pegar) => Some(F4),
 
-        (G1, Cima) => Some(G1),
-        (G1, Direita) => Some(H1),
-        (G1, Esquerda) => Some(F4),
-        (G1, Baixo) => Some(G1),
-        (G1, Pegar) => Some(G1),
+        (G1, Cima) => None,
+        (G1, Direita) => None,
+        (G1, Esquerda) => None,
+        (G1, Baixo) => None,
+        (G1, Pegar) => None,
 
         (G2, Cima) => Some(G3),
         (G2, Direita) => Some(H2),
@@ -281,11 +294,11 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (G2, Baixo) => Some(G1),
         (G2, Pegar) => Some(G2),
 
-        (G3, Cima) => Some(G4),
-        (G3, Direita) => Some(H3),
-        (G3, Esquerda) => Some(F3),
-        (G3, Baixo) => Some(G2),
-        (G3, Pegar) => Some(G3),
+        (G3, Cima) => None,
+        (G3, Direita) => None,
+        (G3, Esquerda) => None,
+        (G3, Baixo) => None,
+        (G3, Pegar) => None,
 
         (G4, Cima) => Some(G4),
         (G4, Direita) => Some(H4),
@@ -311,11 +324,11 @@ fn transição(estado_atual: Estado, simbolo: Símbolo) -> Option<Estado> {
         (H3, Baixo) => Some(H2),
         (H3, Pegar) => Some(H3),
 
-        (H4, Cima) => Some(H4),
-        (H4, Direita) => Some(H4),
-        (H4, Esquerda) => Some(G4),
-        (H4, Baixo) => Some(H3),
-        (H4, Pegar) => Some(H4),
+        (H4, Cima) => None,
+        (H4, Direita) => None,
+        (H4, Esquerda) => None,
+        (H4, Baixo) => None,
+        (H4, Pegar) => None,
     }
 }
 
@@ -330,7 +343,7 @@ impl<'a> Labirinto<'a> {
         }
     }
 
-    fn iter(&'a self, palavra: &'a [String]) -> LabirintoIter<'a> {
+    fn read(&'a self, palavra: &'a [String]) -> LabirintoIter<'a> {
         LabirintoIter {
             palavra,
             estado_atual: self.inicial,
@@ -359,15 +372,23 @@ impl<'a> Iterator for LabirintoIter<'a> {
         let simbolo = match simbolo {
             Ok(s) => s,
             Err(_) => {
-                println!("O símbolo \"{simbolo_str}\" não é reconhecido pelo alfabeto");
-                return None;
+                eprintln!("O símbolo \"{simbolo_str}\" não é reconhecido pelo alfabeto (Palavra rejeitada)");
+                process::exit(-1);
             }
         };
 
         assert!(self.alfabeto.contains(&simbolo));
 
         self.palavra = &self.palavra[1..];
-        let proximo = (self.transição)(self.estado_atual, simbolo)?;
+        let proximo = (self.transição)(self.estado_atual, simbolo);
+
+        let proximo = match proximo {
+            Some(p) => p,
+            None => {
+                println!("δ({:?}, {simbolo:?}) => —", self.estado_atual);
+                return None;
+            }
+        };
 
         assert!(self.estados.contains(&proximo));
 
@@ -396,7 +417,8 @@ fn get_palavra(filepath: String) -> Vec<String> {
     let mut palavra = String::new();
     let _ = file.read_to_string(&mut palavra);
     palavra
-        .split(',')
+        .split(|c: char| c.is_whitespace() || c == ',' )
+        .filter(|c| c != &"")
         .map(|simbolo| simbolo.trim().to_owned())
         .collect()
 }
@@ -413,9 +435,10 @@ fn main() {
             .map(|&c| c.to_owned())
             .collect()
     };
+
     println!("Estado inicial => {:?}", máquina.inicial);
 
-    if let Some(last) = máquina.iter(&palavra).last() {
+    if let Some(last) = máquina.read(&palavra).last() {
         if máquina.finais.contains(&last) {
             println!("Parabéns você achou o tesouro e saiu da caverna!!! (Palavra aceita)");
         } else if last == máquina.inicial {
@@ -426,4 +449,223 @@ fn main() {
     } else {
         println!("Você não fez nada(Rejeitado por preguiça)");
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use crate::{Labirinto, Estado};
+
+    impl<'a> Labirinto<'a> {
+        
+        fn new_inicial(inicial: Estado) -> Self {
+            Self {
+                inicial,
+                .. Self::new()
+            }
+        }
+    }
+    fn split_input(s: &str) -> Vec<String> {
+        s.chars().map(|c| c.to_string()).collect()
+    }
+
+    fn should_accept(palavra: &str, inicial: Estado) {
+        let palavra = split_input(palavra);
+        let maquina = Labirinto::new_inicial(inicial);
+
+        let estado_final = maquina.read(&palavra).last().unwrap();
+        assert!(maquina.finais.contains(&estado_final));
+    }
+
+    fn should_reject(palavra: &str, inicial: Estado) {
+        let palavra = split_input(palavra);
+        let maquina = Labirinto::new_inicial(inicial);
+
+        let estado_final = maquina.read(&palavra).last().unwrap();
+        assert!(!maquina.finais.contains(&estado_final));
+    }
+
+    #[test]
+    fn aceita1() {
+        let palavra = "cdcpbeb";
+        should_accept(palavra, Estado::A1);
+    }
+
+    #[test]
+    fn aceita2() {
+        let palavra = "cdddbccbeecceddebpbbe";
+        should_accept(palavra, Estado::A1);
+    }
+
+    #[test]
+    fn aceita3() {
+        let palavra = "decbcdcceddebbbcddcbbceecpceddebbddcbbceebceb";
+        should_accept(palavra, Estado::A1);
+    }
+
+    #[test]
+    fn aceita4() {
+        let palavra = "ebcebdbcdddcdbbdbceecccdceecedbpbbe";
+        should_accept(palavra, Estado::A1);
+    }
+
+    #[test]
+    fn aceita5() {
+        let palavra = "cdcpceecdcdcebbddcdbbbdceebbceebbe";
+        should_accept(palavra, Estado::A1);
+    }
+
+    #[test]
+    fn rejeita_sair_wumpus1() {
+        let palavra = "d";
+        let inicial = Estado::A1;
+        should_reject(palavra, inicial);
+        let palavra = "b";
+        should_reject(palavra, inicial);
+        let palavra = "c";
+        should_reject(palavra, inicial);
+        let palavra = "e";
+        should_reject(palavra, inicial);
+        let palavra = "p";
+        should_reject(palavra, inicial);
+    }
+
+    #[test]
+    fn rejeita_sair_wumpus2() {
+        let palavra = "d";
+        let inicial = Estado::A1;
+        should_reject(palavra, inicial);
+        let palavra = "b";
+        should_reject(palavra, inicial);
+        let palavra = "c";
+        should_reject(palavra, inicial);
+        let palavra = "e";
+        should_reject(palavra, inicial);
+        let palavra = "p";
+        should_reject(palavra, inicial);
+    }
+
+    // entrou no quadradinho do wumpus antes de sair
+    #[test]
+    fn rejeita_entrar_e_sair_wumpus1() {
+        let palavra = "cdpbbe";
+        let inicial = Estado::A2;
+        should_reject(palavra, inicial);
+
+        let palavra = "edpbbe";
+        let inicial = Estado::B3;
+        should_reject(palavra, inicial);
+
+        let palavra = "bdpbbe";
+        let inicial = Estado::A4;
+        should_reject(palavra, inicial);
+    }
+
+    // Encontrou o tesouro mas aentrou no quadradinho do wumpus
+
+    #[test]
+    fn rejeita_entrar_e_sair_wumpus2() {
+        let palavra = "cbb";
+        let inicial = Estado::E2;
+        should_reject(palavra, inicial);
+
+        let palavra = "ebb";
+        let inicial = Estado::F3;
+        should_reject(palavra, inicial);
+
+        let palavra = "bbb";
+        let inicial = Estado::E4;
+        should_reject(palavra, inicial);
+    }
+
+    #[test]
+    fn rejeita_buraco_c1() {
+        let palavra = "dcecpbbe";
+        let inicial = Estado::B1;
+        should_reject(palavra, inicial);
+
+        let palavra = "bcecpbbe";
+        let inicial = Estado::C2;
+        should_reject(palavra, inicial);
+
+        let palavra = "edceecpbbe";
+        let inicial = Estado::D1;
+        should_reject(palavra, inicial);
+    }
+
+    #[test]
+    fn rejeita_buraco_c3() {
+        let palavra = "depbbe";
+        let inicial = Estado::B3;
+        should_reject(palavra, inicial);
+
+        let palavra = "cbecpbbe";
+        let inicial = Estado::C2;
+        should_reject(palavra, inicial);
+
+        let palavra = "edbeecpbbe";
+        let inicial = Estado::D3;
+        should_reject(palavra, inicial);
+        
+        let palavra = "bcebpbbe";
+        let inicial = Estado::C4;
+        should_reject(palavra, inicial);
+    }
+
+    #[test]
+    fn rejeita_buraco_d4() {
+        let palavra = "cbbeecpbbe";
+        let inicial = Estado::D3;
+        should_reject(palavra, inicial);
+        
+        let palavra = "deebpbbe";
+        let inicial = Estado::C4;
+        should_reject(palavra, inicial);
+    }
+    
+    #[test]
+    fn rejeita_buraco_g1() {
+        let palavra = "dee";
+        let inicial = Estado::F1;
+        should_reject(palavra, inicial);
+
+        let palavra = "bceeb";
+        let inicial = Estado::G2;
+        should_reject(palavra, inicial);
+
+        let palavra = "edceeeb";
+        let inicial = Estado::H1;
+        should_reject(palavra, inicial);
+    }
+
+    #[test]
+    fn rejeita_buraco_g3() {
+        let palavra = "debbe";
+        let inicial = Estado::F3;
+        should_reject(palavra, inicial);
+
+        let palavra = "cbeeb";
+        let inicial = Estado::G2;
+        should_reject(palavra, inicial);
+
+        let palavra = "edbeeeb";
+        let inicial = Estado::H3;
+        should_reject(palavra, inicial);
+        
+        let palavra = "bcebbbe";
+        let inicial = Estado::G4;
+        should_reject(palavra, inicial);
+    }
+
+    #[test]
+    fn rejeita_buraco_h4() {
+        let palavra = "cbbeeeb";
+        let inicial = Estado::H3;
+        should_reject(palavra, inicial);
+        
+        let palavra = "deebbbe";
+        let inicial = Estado::G4;
+        should_reject(palavra, inicial);
+    }
+
 }
